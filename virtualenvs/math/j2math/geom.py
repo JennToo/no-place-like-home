da25576725825f1b779f05a87ae0eba7e7b899af
Jennifer Wilcox <jennifer@nitori.org>
2023-03-20 16:19:29 -0500

    Remove failed calculator experiment

--- virtualenvs/math/j2math/geom.py
+++
@@ -1,4 +0,0 @@
-from math import pi
-
-def circle_area(radius):
-    return pi * radius * radius
--- virtualenvs/math/startup.py
+++
@@ -1,6 +0,0 @@
-from pint import UnitRegistry
-import numpy as np
-from j2math import geom, const
-
-u = UnitRegistry()
-
