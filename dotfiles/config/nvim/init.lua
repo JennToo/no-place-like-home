d37715b6c2ee7ee46ad138a84a71e2f77e3f0825
Jennifer Wilcox <jennifer@nitori.org>
2023-06-30 15:16:50 -0500

    Add window key motions within vim

--- dotfiles/config/nvim/init.lua
+++ dotfiles/config/nvim/init.lua
@@ -26,6 +26,22 @@ Plug 'rose-pine/neovim'
 call plug#end()
 ]], true)
 
+vim.api.nvim_exec(
+[[
+:tnoremap <A-h> <C-\><C-N><C-w>h
+:tnoremap <A-j> <C-\><C-N><C-w>j
+:tnoremap <A-k> <C-\><C-N><C-w>k
+:tnoremap <A-l> <C-\><C-N><C-w>l
+:inoremap <A-h> <C-\><C-N><C-w>h
+:inoremap <A-j> <C-\><C-N><C-w>j
+:inoremap <A-k> <C-\><C-N><C-w>k
+:inoremap <A-l> <C-\><C-N><C-w>l
+:nnoremap <A-h> <C-w>h
+:nnoremap <A-j> <C-w>j
+:nnoremap <A-k> <C-w>k
+:nnoremap <A-l> <C-w>l
+]], true)
+
 vim.opt.termguicolors = true
 vim.opt.clipboard= "unnamedplus"
 vim.opt.number = true
