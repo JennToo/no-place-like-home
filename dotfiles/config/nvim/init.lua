91dcbfb04a8090a338b1440cb760cc01b9896aa4
Jennifer Wilcox <jennifer.wilcox@adtran.com>
2025-04-07 16:17:05 -0500

    Disable line numbers
    I don't really use them anyways. And this gets back 4 chars on width

--- dotfiles/config/nvim/init.lua
+++ dotfiles/config/nvim/init.lua
@@ -52,7 +52,6 @@ pcall(require, "projects/" .. project_name)
 
 vim.opt.termguicolors = true
 vim.opt.clipboard= "unnamedplus"
-vim.opt.number = true
 vim.opt.spelllang="en"
 vim.opt.spellfile="/home/jwilcox/.config/nvim/en.utf-8.add"
 vim.opt.cursorline = true
